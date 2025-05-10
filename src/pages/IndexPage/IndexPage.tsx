6b8a42b6a164f2b8f68dd86db4a9e86a450832d8
Pavel Khomich <paulkhomich@outlook.com>
2025-05-10 23:09:33 +0300

    [master] fix lint

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,7 +1,6 @@
 import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
 import type { FC } from 'react';
 
-import { Link } from '@/components/Link/Link.tsx';
 import { Page } from '@/components/Page.tsx';
 
 import heart from './heart.svg';
